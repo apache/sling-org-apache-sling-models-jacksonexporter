c7db896c2d4871f88869fc54bedffb1cdb5760ad
Stefan Seifert <stefanseifert@users.noreply.github.com>
2021-12-02 10:56:05 +0100

    SLING-10959 Sling Models Exporter: Migrate from Felix SCR annotations to OSGi R7 annotations, Update to Parent 46 (#4)

--- src/main/java/org/apache/sling/models/jacksonexporter/impl/RequestModuleProvider.java
+++ src/main/java/org/apache/sling/models/jacksonexporter/impl/RequestModuleProvider.java
@@ -21,19 +21,14 @@ import java.util.Enumeration;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Property;
-import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.jacksonexporter.ModuleProvider;
-import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
 
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
-@Component
-@Service
-@Property(name = Constants.SERVICE_RANKING, intValue = 0)
+@Component(service = ModuleProvider.class)
 public class RequestModuleProvider implements ModuleProvider {
 
     private final SimpleModule moduleInstance;
@@ -50,4 +45,5 @@ public class RequestModuleProvider implements ModuleProvider {
     public Module getModule() {
         return moduleInstance;
     }
+
 }
