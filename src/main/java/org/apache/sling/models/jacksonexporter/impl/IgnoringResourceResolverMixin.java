783c095f91eeba21a3ab94c702d3103606413efe
Joerg Hoh <joerghoh@apache.org>
2023-07-02 19:41:20 +0200

    cleanup

--- src/main/java/org/apache/sling/models/jacksonexporter/impl/IgnoringResourceResolverMixin.java
+++ src/main/java/org/apache/sling/models/jacksonexporter/impl/IgnoringResourceResolverMixin.java
@@ -18,18 +18,11 @@
  */
 package org.apache.sling.models.jacksonexporter.impl;
 
-import java.io.IOException;
-
 import org.apache.sling.api.resource.ResourceResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreType;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 /**
  * This mixin disables the serialization of the ResourceResolver; if it's still requested, a ERROR log message is written.
@@ -38,28 +31,15 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @JsonIgnoreType
 public abstract interface IgnoringResourceResolverMixin extends ResourceResolver {
+
+    
+    // TODO: find a way how we can both ignore this type and write this warning when trying to serialize a ResourceResolver.    
     
     public static final String MESSAGE = "The serialization of a ResourceResolver was rejected, because the JacksonExporter servlet "
             + "is configured to do so. Please review your Sling Model implementation class(es) and remove any public reference to a "
             + "ResourceResolver."; 
 
     public static final Logger LOG = LoggerFactory.getLogger(JacksonExporter.class);
-    
-    
-    @JsonSerialize(using=JustLoggingSerializer.class)
-    @Override
-    boolean isLive();
-    
 
-    
-    public class JustLoggingSerializer extends JsonSerializer<Boolean> {
-
-        @Override
-        public void serialize(Boolean value, JsonGenerator jgen, SerializerProvider provider)
-                throws IOException, JsonProcessingException {
-            LOG.warn(MESSAGE);
-        }
-        
-    }
     
 }
