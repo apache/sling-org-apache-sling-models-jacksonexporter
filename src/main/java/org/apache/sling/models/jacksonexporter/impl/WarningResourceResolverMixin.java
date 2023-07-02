2e213bc9f3616b842d6a002f4947e609787e4c5e
Joerg Hoh <joerghoh@apache.org>
2023-07-02 19:52:41 +0200

    minor version bump on testing lib, sonar fix

--- src/main/java/org/apache/sling/models/jacksonexporter/impl/WarningResourceResolverMixin.java
+++ src/main/java/org/apache/sling/models/jacksonexporter/impl/WarningResourceResolverMixin.java
@@ -26,7 +26,6 @@ import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
